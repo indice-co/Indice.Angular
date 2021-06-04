5033ff7ab2fe016e7ae84ee76207c07534441a98
Γιάννης Τσενές <itsenes@indice.gr>
2021-06-04 16:57:52 +0300

    removed demo-app

--- projects/demo-app/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: '<lib-shell-layout></lib-shell-layout>'
-})
-export class AppComponent {
-  title = 'demo-app';
-}
