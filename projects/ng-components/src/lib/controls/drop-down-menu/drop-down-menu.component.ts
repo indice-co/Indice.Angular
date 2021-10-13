f247f73c77d76bd48f3bad0b6092a87cd4a279ea
saehon <vaggelanos@yahoo.com>
2021-10-13 16:25:56 +0300

    add placeholder option for dropdown menu

--- projects/ng-components/src/lib/controls/drop-down-menu/drop-down-menu.component.ts
+++ projects/ng-components/src/lib/controls/drop-down-menu/drop-down-menu.component.ts
@@ -4,17 +4,18 @@ import { MenuOption } from '../../types';
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'lib-drop-down-menu',
-  templateUrl: './drop-down-menu.component.html'
+  templateUrl: './drop-down-menu.component.html',
 })
-export class DropDownMenuComponent implements OnInit, OnChanges  {
+export class DropDownMenuComponent implements OnInit, OnChanges {
   @Input() options: MenuOption[] = [];
   // tslint:disable-next-line:no-input-rename
   @Input('selected') selectedValue: any | null = null;
   @Input() multiple = false;
+  @Input() placeholder: string = 'Παρακαλώ επιλέξτε...';
 
   private selectedOption$: MenuOption | null = null;
   public get selectedOption(): MenuOption | null {
-      return this.selectedOption$;
+    return this.selectedOption$;
   }
   public set selectedOption(option: MenuOption | null) {
     this.selectedOption$ = option;
@@ -26,22 +27,21 @@ export class DropDownMenuComponent implements OnInit, OnChanges  {
   @Output() selectedChanged: EventEmitter<any> = new EventEmitter<any>();
 
   private expanded$ = false;
-    public get expanded(): boolean {
-        return this.expanded$;
-    }
-    public set expanded(value: boolean) {
-        this.expanded$ = value;
-    }
-  constructor() { }
+  public get expanded(): boolean {
+    return this.expanded$;
+  }
+  public set expanded(value: boolean) {
+    this.expanded$ = value;
+  }
+  constructor() {}
 
   ngOnChanges(changes: SimpleChanges): void {
     if (this.options && this.options.length > 0 && this.selectedValue !== null && this.selectedValue !== undefined) {
-      this.selectedOption$ = this.options.filter(o => o.value === this.selectedValue)[0];
+      this.selectedOption$ = this.options.filter((o) => o.value === this.selectedValue)[0];
     }
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   public isSelected(option: MenuOption): boolean {
     return option != null && this.selectedValue != null && option.value === this.selectedValue;
@@ -56,5 +56,4 @@ export class DropDownMenuComponent implements OnInit, OnChanges  {
     this.selectedChange.emit(option.value);
     this.selectedChanged.emit(option.value);
   }
-
 }
