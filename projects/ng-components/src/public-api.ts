3476b2fcbf6fbce2c370421deef22d0d1b8da895
Stratos Palaiologos <s.palaiologos@indice.gr>
2022-06-14 17:17:46 +0300

    export stepper-type

--- projects/ng-components/src/public-api.ts
+++ projects/ng-components/src/public-api.ts
@@ -27,6 +27,7 @@ export * from './lib/controls/stepper/lib-stepper.component';
 export * from './lib/controls/stepper/lib-step.component';
 export * from './lib/controls/stepper/lib-step-label.directive';
 export * from './lib/controls/stepper/lib-step-info.directive';
+export * from './lib/controls/stepper/types/stepper-type';
 
 // shell layout
 export * from './lib/layouts/shell/shell-layout/shell-layout.component';
