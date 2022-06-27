5d2313cd677e544fb151683f62fb94b73f9504be
Yiannis Tsenes <itsenes@indice.gr>
2022-06-27 16:06:43 +0300

    navlinkslist component active fragment fix

--- projects/ng-components/src/lib/controls/nav-links-list/nav-links-list.component.ts
+++ projects/ng-components/src/lib/controls/nav-links-list/nav-links-list.component.ts
@@ -1,12 +1,12 @@
-import { Observable } from 'rxjs';
-import { Component, Input, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { NavLink } from '../../types';
 
 @Component({
   selector: 'lib-nav-links-list',
   templateUrl: './nav-links-list.component.html'
 })
-export class NavLinksListComponent implements OnInit {
+export class NavLinksListComponent implements OnInit, OnDestroy {
 
   @Input() links: Observable<NavLink[]> | undefined;
   // tslint:disable-next-line:no-input-rename
@@ -19,9 +19,16 @@ export class NavLinksListComponent implements OnInit {
   @Input('container-class') containerClass: string | string[] | undefined = undefined;
   // tslint:disable-next-line:no-input-rename
   @Input('show-icons') showIcons: boolean | undefined = false;
+  public fragmentValue: string | undefined;
+  private fragmentSub$: Subscription | undefined;
   constructor() { }
+  
+  ngOnDestroy(): void {
+    this.fragmentSub$?.unsubscribe();
+  }
 
   ngOnInit(): void {
+      this.fragmentSub$ = this.activeFragment?.subscribe( fragment => this.fragmentValue = fragment);
   }
 
 }
