fee45968575f4ad11d1b2a50d688285e41c9737d
Yiannis Tsenes <itsenes@indice.gr>
2022-10-05 10:49:41 +0300

    model view layout navigation mobile form factor

--- projects/ng-components/src/lib/layouts/views/model-view-layout/model-view-layout.component.ts
+++ projects/ng-components/src/lib/layouts/views/model-view-layout/model-view-layout.component.ts
@@ -1,13 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-import { HeaderMetaItem, ViewAction } from '../../../types';
+import { HeaderMetaItem, MenuOption, ViewAction } from '../../../types';
+import { ActivatedRoute, NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'lib-model-view-layout',
   templateUrl: './model-view-layout.component.html'
 })
-export class ModelViewLayoutComponent implements OnInit {
+export class ModelViewLayoutComponent implements OnInit, OnDestroy {
   public showRightPaneSM = false;
   @Input() title = 'no title';
   // tslint:disable-next-line:no-input-rename
@@ -21,10 +23,49 @@ export class ModelViewLayoutComponent implements OnInit {
   @Input() icon: string | null = null;
   @Input() busy = false;
   @Input() actions: ViewAction[] | null = null;
-  constructor( private location: Location) { }
+  private optionsLoaded = false;
+  private _options: MenuOption[] = [];
+  public selectedTab: any;
+  private selectedTabSub$: Subscription | undefined;
+  public get tabsOptions(): MenuOption[] {
+    if(!this.optionsLoaded) {
+      this.optionsLoaded = true;
+      if(this.primary) {
+        this.primary.forEach(p => {
+          this._options.push(new MenuOption(p.text, p.link));
+        });
+      }
 
-  ngOnInit(): void {
+      if(this.secondary) {
+        this.secondary.forEach(p => {
+          this._options.push(new MenuOption(p.text, p.link));
+        });
+      }
+      this.selectedTab = this._options[0].value; 
+    }
+    return this._options;
   }
+  constructor( private location: Location, private router: Router, private route: ActivatedRoute) { }
+  
+  ngOnInit(): void {
+    this.selectedTabSub$ = this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd ) {
+        if(event.urlAfterRedirects) {
+          var urlParts = event.urlAfterRedirects.split('/');
+          if(urlParts && urlParts.length > 0) {
+            this.selectedTab = urlParts[urlParts.length-1];
+          }
+        }
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if(this.selectedTabSub$) {
+      this.selectedTabSub$.unsubscribe();
+    }
+  }
+
 
   public onSidePaneActivated($event: any): void  {
     this.showRightPaneSM = true;
@@ -36,5 +77,9 @@ export class ModelViewLayoutComponent implements OnInit {
 
   public closeSidePane(): void {
     this.location.back();
+  }
+
+  public navigateLink(link: any): void {
+    this.router.navigate([link], {relativeTo: this.route});
   }
 }
