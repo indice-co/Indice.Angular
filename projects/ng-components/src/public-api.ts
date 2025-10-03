9008c7ea9dff7fe7ad6cabb1f1d1fcf6755687de
Vasilis Spyridonos <spyridonosvasileios@gmail.com>
2025-10-03 18:48:34 +0300

    add missing export for step-selected-event in public-api.ts

--- projects/ng-components/src/public-api.ts
+++ projects/ng-components/src/public-api.ts
@@ -32,6 +32,7 @@ export * from './lib/controls/stepper/lib-step.component';
 export * from './lib/controls/stepper/lib-step-label.directive';
 export * from './lib/controls/stepper/lib-step-info.directive';
 export * from './lib/controls/stepper/types/stepper-type';
+export * from './lib/controls/stepper/types/step-selected-event';
 export * from './lib/controls/toggle-button/toggle-button.component';
 export * from './lib/controls/toggle-buttons-list/toggle-buttons-list.component';
 export * from './lib/controls/content-tile/content-tile.component';
